676fc81cf7c65f4eeb8aa93d9b12689029f7b643
shriya-gudkaa <gudkashriya@gmail.com>
2026-02-28 18:09:07 +0530

    Updated array creation file - Added 2D arrays

--- numpy/array_creation.py
+++ numpy/array_creation.py
@@ -28,3 +28,19 @@ print("Range array:\n",arange_arr)
 
 arange_arr1 = np.arange(1,11,2)
 print("array created with arange start-stop-step:\n",arange_arr1)
+
+# 5. Create 2D array from list of lists
+list_2d = [[1,2,3,4],
+           [5,6,7,8]]
+
+array_2d = np.array(list_2d)
+print("2D Array from list of lists:\n", array_2d)
+
+
+# 6. Create 2D zeros and ones arrays
+
+zeros_2d = np.zeros((2, 3))
+print("2D Zeros array:\n", zeros_2d)
+
+ones_2d = np.ones((3, 2))
+print("2D Ones array:\n", ones_2d)
