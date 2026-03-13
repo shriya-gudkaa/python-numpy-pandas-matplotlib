d50a0035984b3e50a70670084a042d24943d2e9c
shriya-gudkaa <gudkashriya@gmail.com>
2026-03-13 18:06:51 +0530

    Improved pandas modifying_dataframe.py with clear explanatory comments

--- pandas/Modifying_dataframe.py
+++ pandas/Modifying_dataframe.py
@@ -1,26 +1,49 @@
-
 import pandas as pd
 import numpy as np
 
-d = {'EmpId' : ['E01','E02','E03','E04'],    
-'EmpName' : ['Raj','Atul','Reena','Ayushi'],    
-'Department' : ['IT','IT','HR','Accounts']}    
+# Creating a dictionary containing employee information
+d = {
+    'EmpId': ['E01','E02','E03','E04'],
+    'EmpName': ['Raj','Atul','Reena','Ayushi'],
+    'Department': ['IT','IT','HR','Accounts']
+}
+
+# Creating a DataFrame from the dictionary
+# Custom row labels (index) are also provided
 df = pd.DataFrame(d, index=['First','Second','Third','Fourth'])
-print("df:\n", df)
+
+print("Initial DataFrame:\n", df)
 print()
-#Adding a Row
-# “loc” attribute is also used to add a new row in DataFrame
-df.loc['Fifth', : ] = ['E05', 'Nakul', 'HR']
+
+# Adding rows to the DataFrame
+
+# The loc[] attribute can be used to add a new row
+
+df.loc['Fifth', :] = ['E05', 'Nakul', 'HR']
+print("After adding 'Fifth' row:")
 print(df)
 
-df.loc['Sixth']=['E06', 'Rahul', 'Accounts']
+# Adding another row using loc
+df.loc['Sixth'] = ['E06', 'Rahul', 'Accounts']
+print("After adding 'Sixth' row:")
 print(df)
 
-# Modifying a Row
+# Modifying or adding a value in a specific column
+
+# Here we assign a value to the EmpId column for a new row label
+# If the row label does not exist, Pandas will create it
 df.EmpId['Seventh'] = 'E07'
+
+print("After modifying/adding 'Seventh' row:")
 print(df)
 
-# Adding a colum
-#df['City'] = 'New Delhi'
-df['City'] = ['New Delhi', np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]
+
+# Adding a new column to the DataFrame
+
+# Creating a new column named 'City'
+# np.nan is used to represent missing values
+
+df['City'] = ['New Delhi', np.nan, np.nan, np.nan, np.nan, np.nan]
+
+print("After adding 'City' column:")
 print(df)
