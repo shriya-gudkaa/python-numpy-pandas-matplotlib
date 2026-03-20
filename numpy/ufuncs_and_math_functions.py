a6223a60b13ee5fff3c3e31397be2c8f175ab03b
shriya-gudkaa <gudkashriya@gmail.com>
2026-03-20 18:52:04 +0530

    Added comparison functions

--- numpy/ufuncs_and_math_functions.py
+++ numpy/ufuncs_and_math_functions.py
@@ -5,17 +5,17 @@ Description: Mathematical operations applied element-wise on arrays
 
 import numpy as np
 
-arr = np.array([1, 4, 9, 16])
-print("Original Array:", arr)
+arr1 = np.array([1, 4, 9, 16])
+print("Original Array:", arr1)
 
 # 1. Square root
-print("Square root: ",np.sqrt(arr))
+print("Square root: ",np.sqrt(arr1))
 
 # 2. Exponential
-print("Exponential (e^x):", np.exp(arr))
+print("Exponential (e^x):", np.exp(arr1))
 
 # 3. Power
-print("Power: ",np.power(arr,3))
+print("Power: ",np.power(arr1,3))
 
 # 4. Rounding Functions
 arr_round = np.array([1.2, 2.5, 3.7, 4.5])
@@ -27,3 +27,13 @@ print("Ceil:", np.ceil(arr_round))
 # 5 Absolute Value
 arr_abs = np.array([-5, -2, 0, 3])
 print("Absolute Values:", np.abs(arr_abs))
+
+# 6 Comparison Functions
+arr2 = np.array([4, 2, 1 ,9])
+print("\nComparison Operations:")
+
+print("Greater (arr1 > arr2):", np.greater(arr1, arr2))
+
+print("Less (arr1 < arr2):", np.less(arr1, arr2))
+
+print("Equal (arr1 == arr2):", np.equal(arr1, arr2))
